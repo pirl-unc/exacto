42154dd9af12a9ac5daabf9ab8efe09ebae595ff
Andy Jinseok Lee <andyjinseoklee@gmail.com>
2024-02-09 22:06:07 -0500

    test v0.3.0

--- test/test_identify.py
+++ test/test_identify.py
@@ -1,12 +1,13 @@
+from .data import get_data_path
 from exactolib.main import identify_rna_variants
 
 
 def test_identify_1():
     variant_calls = identify_rna_variants(
-        bam_file='/Users/leework/Documents/Research/projects/project_exacto/exacto/test/data/hg38_tp53_rna_minimap2_mdtagged_sorted.bam',
-        min_reads=1,
+        bam_file=get_data_path(name='hg38_tp53_rna_minimap2_mdtagged_sorted.bam'),
+        min_reads=3,
         min_mapping_quality=20,
-        num_threads=2,
+        num_threads=1,
         chromosomes=['chr17']
     )
     print(len(variant_calls))
